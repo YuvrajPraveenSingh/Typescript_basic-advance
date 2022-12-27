6000f09726915c2e67d7c3ef43ca9bcf2a16461b
yuv00191 <>
2022-12-27 13:14:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//  basic console log
+let no = 12;
+console.log(no);
+let s  = (no >10 && no<20)?"no is btw 10 and 20 ": "no is not btw 10 and 20 ";
+console.log(s)
+console.log(__filename);
+
+// set intervels 
+setInterval(()=>{
+  console.log("hello there set intervel time is 1 sec")
+}, 1000)
+// Modules
